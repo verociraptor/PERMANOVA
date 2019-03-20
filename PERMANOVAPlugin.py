d398f5290aa8fef1982b2c402ee84a55f0074661
verociraptor <veronica.parra1@fiu.edu>
2019-03-20 13:24:20 -0400

    Initial commit

---
+++ PERMANOVAPlugin.py
@@ -0,0 +1,33 @@
+import skbio
+import csv
+
+
+class PERMANOVAPlugin:
+
+	def input(self, filename):
+		self.myfile = filename
+		
+
+	def run(self):
+		files = open(self.myfile, "r")
+		filenames = files.read().split()
+		files.close()
+		
+
+		self.groups = open("plugins/ANOSIM/example/"+filenames[1], "r")
+		
+		#get group names from groupfile
+		csv_reader = csv.reader(self.groups)
+		group_names = []
+		next(csv_reader)
+
+		for row in csv_reader:
+			group_names.append(row[1])
+
+		#get distance matrix from distfile
+		dm = skbio.DistanceMatrix.read("plugins/PERMANOVA/example/"+filenames[3], format='lsmat')
+		self.result = skbio.stats.distance.permanova(dm, group_names)
+		
+
+	def output(self, filename):
+		print self.result

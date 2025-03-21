ed89d876d0f9ff3da8c20004ef49ac06b5733416
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-03-21 13:22:46 -0400

    Fixed input directory

--- PERMANOVAPlugin.py
+++ PERMANOVAPlugin.py
@@ -1,35 +1,32 @@
-import numpy
-numpy.random.seed(1234)
+#import numpy
+#numpy.random.seed(1234)
 import skbio
 import csv
-
+import PyPluMA
 
 class PERMANOVAPlugin:
+    def input(self, filename):
+        self.myfile = filename
 
-	def input(self, filename):
-		self.myfile = filename
-		
+    def run(self):
+        files = open(self.myfile, "r")
+        filenames = files.read().split()
+        files.close()
 
-	def run(self):
-		files = open(self.myfile, "r")
-		filenames = files.read().split()
-		files.close()
-		
+        self.groups = open(PyPluMA.prefix()+"/"+filenames[1], "r")
 
-		self.groups = open("plugins/ANOSIM/example/"+filenames[1], "r")
-		
-		#get group names from groupfile
-		csv_reader = csv.reader(self.groups)
-		group_names = []
-		next(csv_reader)
+        #get group names from groupfile
+        csv_reader = csv.reader(self.groups)
+        group_names = []
+        next(csv_reader)
 
-		for row in csv_reader:
-			group_names.append(row[1])
+        for row in csv_reader:
+            group_names.append(row[1])
 
-		#get distance matrix from distfile
-		dm = skbio.DistanceMatrix.read("plugins/PERMANOVA/example/"+filenames[3], format='lsmat')
-		self.result = skbio.stats.distance.permanova(dm, group_names)
-		
+        #get distance matrix from distfile
+        dm = skbio.DistanceMatrix.read(PyPluMA.prefix()+"/"+filenames[3], format='lsmat')
+        self.result = skbio.stats.distance.permanova(dm, group_names)
 
-	def output(self, filename):
-		print(self.result)
+    def output(self, filename):
+        outfile = open(filename, 'w')
+        outfile.write(str(self.result))
